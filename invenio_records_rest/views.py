aaf469946242eea995df0e9896539376d3db762f
Leonardo Rossi <leonardo.r@cern.ch>
2016-04-28 16:02:49 +0200

    views: fix record post permissions
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- invenio_records_rest/views.py
+++ invenio_records_rest/views.py
@@ -293,7 +293,7 @@ def verify_record_permission(permission_factory, record):
     """
     # Note, cannot be done in one line due overloading of boolean
     # operations permission object.
-    if not permission_factory(record).can():
+    if not permission_factory(record=record).can():
         from flask_login import current_user
         if not current_user.is_authenticated:
             abort(401)
@@ -307,7 +307,8 @@ def need_record_permission(factory_name):
     """
     def need_record_permission_builder(f):
         @wraps(f)
-        def need_record_permission_decorator(self, record, *args, **kwargs):
+        def need_record_permission_decorator(self, record=None, *args,
+                                             **kwargs):
             permission_factory = (
                 getattr(self, factory_name) or
                 getattr(current_records_rest, factory_name)
@@ -443,6 +444,7 @@ class RecordsListResource(ContentNegotiatedMethodView):
             item_links_factory=self.item_links_factory,
         )
 
+    @need_record_permission('create_permission_factory')
     def post(self, **kwargs):
         """Create a record.
 
@@ -455,17 +457,17 @@ class RecordsListResource(ContentNegotiatedMethodView):
         if data is None:
             abort(400)
 
+        # Check permissions
+        permission_factory = self.create_permission_factory
+        if permission_factory:
+            verify_record_permission(permission_factory, data)
+
         # Create uuid for record
         record_uuid = uuid.uuid4()
         # Create persistent identifier
         pid = self.minter(record_uuid, data=data)
         # Create record
         record = self.record_class.create(data, id_=record_uuid)
-
-        # Check permissions
-        permission_factory = self.create_permission_factory
-        if permission_factory:
-            verify_record_permission(permission_factory, record)
 
         db.session.commit()
 
