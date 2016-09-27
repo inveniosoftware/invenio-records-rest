ed2149d78151ea2c5bffdc67378de3b58c46594f
Jiri Kuncar <jiri.kuncar@cern.ch>
2016-09-27 14:24:23 +0200

    docs: ReadTheDocs in intersphinx config
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -330,6 +330,10 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
-    'invenio-access': ('http://pythonhosted.org/invenio-access', None),
-    'invenio-search': ('http://pythonhosted.org/invenio-search', None),
+    'invenio-access': (
+        'https://invenio-access.readthedocs.io/en/latest/', None
+    ),
+    'invenio-search': (
+        'https://invenio-search.readthedocs.io/en/latest/', None
+    ),
 }
