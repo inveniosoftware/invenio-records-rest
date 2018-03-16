cfad174eed3fcfb222416fd94ace56d4dea3c20c
Invenio <info@inveniosoftware.org>
2018-03-16 15:46:10 +0100

    installation: removed pytest-cache dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ tests_require = [
     'invenio-indexer>=1.0.0a10',
     'isort>=4.3.1',
     'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
     'pytest>=3.3.0',
