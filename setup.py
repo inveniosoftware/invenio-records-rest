11721092de8289b09ae87c3f9f48a6493325cd6d
Niklas Persson <n.persson@cern.ch>
2019-07-26 11:09:32 +0200

    travis: bump es versions and update matrix

--- setup.py
+++ setup.py
@@ -62,8 +62,7 @@ extras_require = {
 
 extras_require['all'] = []
 for name, reqs in extras_require.items():
-    if name[0] == ':' or name in ('elasticsearch5', 'elasticsearch6',
-                                  'elasticsearch7'):
+    if name[0] == ':' or name in ('elasticsearch5', 'elasticsearch6', 'elasticsearch7'):
         continue
     extras_require['all'].extend(reqs)
 
