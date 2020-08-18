eec24d398a7b1e935c698b02123f98b5e76b0562
Pablo Panero <pablo.panero@cern.ch>
2020-08-18 13:49:02 +0200

    travis: update docker compose

--- run-tests.sh
+++ run-tests.sh
@@ -7,11 +7,12 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker-compose --env-file=./.env up -d ${DB} ${ES} ${CACHE}
-sleep 60s
+docker-services-cli services up es postgresql redis
 # pydocstyle invenio_records_rest tests docs && \
 # isort invenio_records_rest tests --check-only --diff && \
 # check-manifest --ignore ".travis-*" && \
 # sphinx-build -qnNW docs docs/_build/html && \
 pytest
-docker-compose --env-file=./.env down
+tests_exit_code=$?
+docker-services-cli services down
+exit "$tests_exit_code"
