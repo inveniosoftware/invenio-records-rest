61190ea21f129c5fd586fe7dceb16a1d81d104b8
Sam Arbid <samaa2@kth.se>
2026-08-27 13:19:30 +0200

    📦 release: v5.1.1

--- invenio_records_rest/__init__.py
+++ invenio_records_rest/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2015-2023 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
-# SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2025-2026 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
 """REST API for Records.
@@ -708,6 +708,6 @@ by Invenio-PIDStore.
 from .ext import InvenioRecordsREST
 from .proxies import current_records_rest
 
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 
 __all__ = ("__version__", "current_records_rest", "InvenioRecordsREST")
